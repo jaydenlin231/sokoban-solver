254fb5e8ea02e56e4b73ababb8a7b379f300d747
jaydenlin231 <jaydenlin231@gmail.com>
2022-04-24 20:11:31 +1000

    Started working on taboo cell search. Finished rule 1

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -29,12 +29,12 @@ Last modified by 2022-03-27  by f.maire@qut.edu.au
 # You have to make sure that your code works with 
 # the files provided (search.py and sokoban.py) as your code will be tested 
 # with these files
+import itertools
 import search 
 import sokoban
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 
 def my_team():
     '''
@@ -46,6 +46,162 @@ def my_team():
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class Floor(search.Problem):
+    def __init__(self, warehouse):
+        self.warehouse = sokoban.Warehouse.copy(warehouse)
+        self.initial = warehouse.worker
+        self.goal = None
+
+    def actions(self, state):
+        """
+        Return the list of actions that can be executed in the given state.
+        
+        """
+        # list of legal actions
+        L = []
+        # get current state worker coords
+        [worker_x, worker_y] = state 
+        # check if action results in the worker on top of a wall, if not, append to legal action
+        # target and box coords are ignored for this problem of finding inside wall area.
+        if(worker_x + 1, worker_y) not in self.warehouse.walls:
+            L.append("Right")
+        if(worker_x - 1, worker_y) not in self.warehouse.walls:
+            L.append("Left")
+        if(worker_x, worker_y + 1) not in self.warehouse.walls:
+            L.append("Down")
+        if(worker_x, worker_y - 1) not in self.warehouse.walls:
+            L.append("Up")
+        
+        return L
+
+    def result(self, state, action):
+        """
+        Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state).
+        """
+
+        next_worker_coords = list(state)
+        
+        assert action in self.actions(state)  # defensive programming!
+        if action == 'Up':
+            next_worker_coords[1] = next_worker_coords[1] - 1
+        if action == 'Down':
+            next_worker_coords[1] = next_worker_coords[1] + 1
+        if action == 'Left':
+            next_worker_coords[0] = next_worker_coords[0] - 1
+        if action == 'Right':
+            next_worker_coords[0] = next_worker_coords[0] + 1
+
+        # self.warehouse = sokoban.Warehouse.copy(self.warehouse, worker=next_worker_coords)
+
+        return tuple(next_worker_coords)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def floor_breadth_first_graph_search(problem):
+    "Graph search version of BFS.  [Fig. 3.11]"
+    return floor_graph_search(problem, search.FIFOQueue())
+
+def floor_graph_search(problem, frontier):
+    """
+    Search through the successors of a problem to find a goal.
+    The argument frontier should be an empty queue.
+    If two paths reach a state, only use the first one. [Fig. 3.7]
+    Return
+        the node of the first goal state found
+        or None is no goal state is found
+    """
+    assert isinstance(problem, search.Problem)
+    frontier.append(search.Node(problem.initial))
+    explored = set() # initial empty set of explored states
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        explored.add(node.state)
+        # Python note: next line uses of a generator
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored
+                        and child not in frontier)
+    return explored
+
+def get_floor_cells(floor):
+    return floor_breadth_first_graph_search(floor)
+    
+def has_up_neighbour(cell, allCells):
+    return (cell[0], cell[1] - 1) in allCells
+
+def has_down_neighbour(cell, allCells):
+    return (cell[0], cell[1] + 1) in allCells
+
+def has_right_neighbour(cell, allCells):
+    return (cell[0] + 1, cell[1]) in allCells
+
+def has_left_neighbour(cell, allCells):
+    return (cell[0] - 1, cell[1]) in allCells
+
+def has_any_neighbour(cell, allCells):
+    return has_up_neighbour(cell, allCells) \
+           or has_down_neighbour(cell, allCells)\
+           or has_right_neighbour(cell, allCells)\
+           or has_left_neighbour(cell, allCells)
+
+def is_bot_left_corner(cell, allCells):
+    return has_up_neighbour(cell, allCells) \
+           and has_right_neighbour(cell, allCells) \
+           and not has_down_neighbour(cell, allCells) \
+           and not has_left_neighbour(cell, allCells) 
+           
+def is_bot_right_corner(cell, allCells):
+    return has_up_neighbour(cell, allCells) \
+           and has_left_neighbour(cell, allCells) \
+           and not has_down_neighbour(cell, allCells) \
+           and not has_right_neighbour(cell, allCells) 
+           
+def is_top_left_corner(cell, allCells):
+    return has_right_neighbour(cell, allCells) \
+           and has_down_neighbour(cell, allCells) \
+           and not has_left_neighbour(cell, allCells) \
+           and not has_up_neighbour(cell, allCells)
+
+def is_top_right_corner(cell, allCells):
+    return has_left_neighbour(cell, allCells) \
+           and has_down_neighbour(cell, allCells) \
+           and not has_right_neighbour(cell, allCells) \
+           and not has_up_neighbour(cell, allCells)
+
+def is_corner(cell, allCells):
+    return is_bot_left_corner(cell, allCells) \
+           or is_bot_right_corner(cell, allCells) \
+           or is_top_right_corner(cell, allCells) \
+           or is_top_left_corner(cell, allCells) 
+
+# def get_floor_area_corners(floor_cells):
+#     return {cell for cell in floor_cells if is_corner(cell, floor_cells)}
+
+get_floor_area_corners = lambda floor_cells : {cell for cell in floor_cells if is_corner(cell, floor_cells)}
+
+are_inline_vert = lambda cellA, cellB : cellA[0] == cellB[0]
+are_inline_hor = lambda cellA, cellB : cellA[1] == cellB[1]
+
+are_in_line = lambda cellA, cellB : are_inline_hor(cellA, cellB) or are_inline_vert(cellA, cellB)
+
+def space_in_line(cellA, cellB):
+    if are_inline_hor(cellA, cellB):
+        y = cellA[1]
+        x_start = min(cellA[0], cellB[0]) + 1
+        x_end = max(cellA[0], cellB[0])
+        if(x_start < x_end and x_start > 0):
+            return {(x, y) for x in range(x_start, x_end)}
+    elif are_inline_vert(cellA, cellB):
+        x = cellA[0]
+        y_start = min(cellA[1], cellB[1]) + 1
+        y_end = max(cellA[1], cellB[1])
+        if(y_start < y_end and y_start > 0):
+            return {(x, y) for y in range(y_start, y_end)}
 
 
 def taboo_cells(warehouse):
@@ -73,8 +229,42 @@ def taboo_cells(warehouse):
        The returned string should NOT have marks for the worker, the targets,
        and the boxes.  
     '''
-    ##         "INSERT YOUR CODE HERE"    
-    raise NotImplementedError()
+    floor_area = get_floor_cells(Floor(warehouse))
+    rule_1 = {cell for cell in get_floor_area_corners(floor_area) if cell not in warehouse.targets}
+    # Prepare results
+    X,Y = zip(*warehouse.walls) 
+    x_size, y_size = 1+max(X), 1+max(Y)
+    
+    vis = [[" "] * x_size for y in range(y_size)]
+
+    
+    rule_2 = set()
+    for combination in itertools.combinations(rule_1, 2):
+        cellA = combination[0]
+        cellB = combination[1]
+        if not are_in_line(cellA, cellB):
+            continue
+
+        if(not space_in_line(cellA, cellB)):
+            continue
+        print("---------------------")
+        # print(f"{cellA} and {cellB}")
+        # print("space in line")
+        # print(space_in_line(cellA, cellB))
+        for space in space_in_line(cellA, cellB):
+            if(not(space in floor_area and has_any_neighbour(space, warehouse.walls))):
+                continue
+
+        for space in space_in_line(cellA, cellB):
+            rule_2.add(space)    
+            
+    for (x,y) in warehouse.walls:
+        vis[y][x] = "#"
+    for (x,y) in (rule_2):
+        vis[y][x] = "X"
+
+    # print(get_floor_area_corners(floor_area))
+    return "\n".join(["".join(line) for line in vis])
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -170,6 +360,19 @@ def solve_weighted_sokoban(warehouse):
     '''
     
     raise NotImplementedError()
+
+if __name__ == "__main__":
+    wh = sokoban.Warehouse()
+    wh.load_warehouse("./warehouses/warehouse_03.txt")
+    
+    print(wh)
+    # print("\n")
+    print(taboo_cells(wh))
+    # for i in space_in_line((1, 2), (1, 10)):
+    #     if(i):
+    #         print(type(i))
+    #         print(i)
+
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
