07be0153c289bea1c15583da877fc6d4482ce712
Alexander <kimtaei2002@gmail.com>
2022-05-03 15:49:18 +1000

    test script to compare with task sheet

--- testScript.py
+++ testScript.py
@@ -5,10 +5,6 @@ import sokoban
 import mySokobanSolver
 
 if __name__ == "__main__":
-    # warehouse = sokoban.Warehouse()
-    # warehouse.load_warehouse("./warehouses/warehouse_07.txt")
-    # print(mySokobanSolver.taboo_cells(warehouse))
-
     test = ["./warehouses/warehouse_07.txt", 
             "./warehouses/warehouse_09.txt",
             "./warehouses/warehouse_47.txt",
@@ -20,5 +16,15 @@ if __name__ == "__main__":
         warehouse = sokoban.Warehouse()
         warehouse.load_warehouse(test[i])
         print("-------------------------------------")
-        print(mySokobanSolver.taboo_cells(warehouse))
+        print(test[i])
+        t0 = time.time()
+        solution, total_cost = mySokobanSolver.solve_weighted_sokoban(warehouse)
+        t1 = time.time()
+        print (f'\nAnalysis took {t1-t0:.6f} seconds\n')
+        if solution == 'impossible':
+            print('\nNo solution found!\n')
+        else:
+            print(f"\nSolution found with a cost of {total_cost} \n", solution, '\n')
+
+        
         
